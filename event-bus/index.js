94502ebbb5c990ae71a70a5d3cad23dcf8349940
walid klai <walid.klai@gmail.com>
2021-09-28 14:23:52 +0100

    to be removed

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,1 +1,25 @@
+<<<<<<< HEAD
 node_modules
+=======
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 4005
+
+app.post("/events",async(req,res)=>{
+    const event = req.body
+    await axios.post("http://localhost:4000/events",event)
+    await axios.post("http://localhost:4001/events",event)
+    await axios.post("http://localhost:4002/events",event)
+    await axios.post("http://localhost:4003/events",event)
+    res.sendStatus(201)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+>>>>>>> parent of 3d61614 (end of second vid)
