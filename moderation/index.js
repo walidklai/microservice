80e4bc6230e5305c2b3032be69e86a1ca081384b
walid klai <walid.klai@gmail.com>
2021-09-28 14:32:42 +0100

    back

--- moderation/index.js
+++ moderation/index.js
@@ -1,1 +1,31 @@
-node_modules
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/events",async(req,res)=>{
+    console.log(`Recieved event: ${req.body.type}`)
+    const {type,data}=req.body
+
+    if(type==="CommentCreated"){
+        const status = data.content.includes("orange") ?"rejected":"approved"
+        await axios.post("http://localhost:4005/events",{
+            type:"CommentModerated",
+            data:{
+                id:data.id,
+                postId:data.postId,
+                status,
+                content:data.content
+            }
+        })
+    }
+    res.sendStatus(200)
+})
+
+const PORT = process.env.PORT || 4003
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
